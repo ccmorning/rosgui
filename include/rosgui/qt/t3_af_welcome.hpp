146ebb9630c7d212fd8595a9c446cdb614cfefea
wings0728 <36744020@qq.com>
2018-01-10 12:14:34 +0800

    first

---
+++ include/rosgui/qt/t3_af_welcome.hpp
@@ -0,0 +1,32 @@
+#ifndef T3_AF_WELCOME_H
+#define T3_AF_WELCOME_H
+
+//#include "t3_af_config.h"
+#include <QDialog>
+
+//#include "t3_af_login.h"
+//#include "t3_af_mainwindow.h"
+
+namespace Ui {
+class T3_AF_welcome;
+}
+
+class T3_AF_welcome : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit T3_AF_welcome(QWidget *parent = 0);
+    ~T3_AF_welcome();
+
+
+
+private Q_SLOTS:
+    void timeUpdate();
+    void enterSystem();
+
+private:
+    Ui::T3_AF_welcome *ui;
+};
+
+#endif // T3_AF_WELCOME_H
---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+/**
+ * @file /src/main.cpp
+ *
+ * @brief Qt based gui.
+ *
+ * @date November 2010
+ **/
+/*****************************************************************************
+** Includes
+*****************************************************************************/
+
+#include <QtGui>
+#include <QApplication>
+#include "../include/rosgui/qt/main_window.hpp"
+#include "../include/rosgui/qt/t3_af_welcome.hpp"
+
+/*****************************************************************************
+** Main
+*****************************************************************************/
+
+int main(int argc, char **argv) {
+
+    /*********************
+    ** Qt
+    **********************/
+    QApplication app(argc, argv);
+    rosgui::MainWindow w(argc,argv);
+    w.show();
+    T3_AF_welcome aa;
+    aa.show();
+    app.connect(&app, SIGNAL(lastWindowClosed()), &app, SLOT(quit()));
+    int result = app.exec();
+
+	return result;
+}
